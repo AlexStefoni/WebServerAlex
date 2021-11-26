1472c340e8fd5f109d0a6e838c8395018b471a0c
BoredOctopus <62361292+AlexStefoni@users.noreply.github.com>
2021-11-26 12:57:14 +0200

    Server version 0.1
    First working server serving a hardcoded html to a single  client

--- src/main/java/Server/config/ConfigurationManager.java
+++ src/main/java/Server/config/ConfigurationManager.java
@@ -1,5 +1,13 @@
 package Server.config;
 
+import Server.config.util.Json;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
     private static ConfigurationManager myConfigurationManager;
     private static Configuration myCurrentConfiguration;
@@ -15,15 +23,43 @@ public class ConfigurationManager {
     * used to load a configuration file
     *
     * */
-    public void loadConfigFile(String filePath){
-
+    public void loadConfigFile(String filePath)  {
+        FileReader fileReader= null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfurationException(e);
+        }
+        StringBuffer sb=new StringBuffer();
+        int i;
+        try {
+            while ((i = fileReader.read()) != -1) {
+                sb.append((char) i);
+            }
+        }catch (IOException e){
+            throw new HttpConfurationException(e);
+        }
+        JsonNode conf= null;
+        try {
+            conf = Json.parse(sb.toString());
+        } catch (JsonProcessingException e) {
+            throw new HttpConfurationException("error parsing the configuration file ",e);
+        }
+        try {
+            myCurrentConfiguration=Json.fromJson(conf,Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new HttpConfurationException("error pasing the configuratio file internal",e);
+        }
     }
     /**
      *
      * retuns curent loaded configurations
      * */
-    public void getCurrentConfig(){
-
+    public Configuration getCurrentConfig(){
+        if (myCurrentConfiguration==null){
+            throw new HttpConfurationException("no curretn configuration set");
+        }
+        return myCurrentConfiguration;
 
     }
 }
---
+++ src/main/java/Server/config/HttpConfurationException.java
@@ -0,0 +1,22 @@
+package Server.config;
+
+public class HttpConfurationException extends  RuntimeException {
+    public HttpConfurationException() {
+    }
+
+    public HttpConfurationException(String message) {
+        super(message);
+    }
+
+    public HttpConfurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public HttpConfurationException(Throwable cause) {
+        super(cause);
+    }
+
+    public HttpConfurationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
