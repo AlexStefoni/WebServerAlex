a32f4ffc737a3b00e33164a9ab4359b0e26602ff
BoredOctopus <62361292+AlexStefoni@users.noreply.github.com>
2021-11-27 01:50:17 +0200

    Create ServerListenerThreadTest.java

---
+++ src/test/java/Server/core/ServerListenerThreadTest.java
@@ -0,0 +1,12 @@
+package Server.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerListenerThreadTest {
+
+    @Test
+    void run() {
+    }
+}
